3f262b01ee0953239bc5ae5c1b618ac2755a1ca7
zqm02 <101574538+zqm02@users.noreply.github.com>
2023-03-07 23:05:06 +0800

    no message

---
+++ 02.项目准备part1/e-ui/src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <el-row>
+      <el-button @click="handleClick">默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+
+      <div class="table-container">
+        <el-table :data="tableData" stripe border>
+          <el-table-column prop="date" label="日期" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </div>
+
+      <div class="radio-container">
+        <el-radio v-model="radio" label="1">备选项</el-radio>
+        <el-radio v-model="radio" label="2">备选项</el-radio>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      window.alert("this is a test");
+    },
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      radio: "1",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.table-container {
+  width: 800px;
+  margin-left: calc(50% - 400px);
+  margin-top: 10px;
+}
+</style>
